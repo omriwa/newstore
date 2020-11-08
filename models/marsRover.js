febe19e502ba49073708e3d5f03bee4c405311f2
omri wallach <omriwallach1992@gmail.com>
2020-11-08 11:20:19 +0100

    implement get mars rover coordinates

--- models/marsRover.js
+++ models/marsRover.js
@@ -2,6 +2,10 @@ class MarsRover {
     constructor(coordinates) {
         this.coordinates = coordinates;
     }
+
+    getCoordinates() {
+        return this.coordinates;
+    }
 }
 
 module.exports = {
